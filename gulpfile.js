f117fe85c8f808938f530bb683f908a2a73b9c8c
João Rigotti <joao.webdev@gmail.com>
2016-10-30 21:05:24 -0200

    Adicionado bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ var js = [
   'js/app.js'
 ];
 
+var css = [
+  'assets/components/bootstrap/dist/css/bootstrap.min.css',
+  'css/app-styles.css'
+];
+
 gulp.task('default', function() {
+  gulp.src(css).pipe(concat('styles.min.css')).pipe(gulp.dest('./css/'));
   gulp.src(js).pipe(concat('bundle.min.js')).pipe(gulp.dest('./js/'));
 });
